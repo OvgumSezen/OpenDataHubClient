1ae200e2b8746eebe598b9b913dbe0e7c3374189
OvgumSezen <ovgumcansezen@gmail.com>
2025-02-04 12:27:19 +0100

    Removed unnecessary file.
    Note: Could not push the tests due to some problems with the JUnit dependencies.

--- src/main/java/MobilityRepository.java
+++
@@ -1,8 +0,0 @@
-import repository.ODHRepository;
-
-import java.util.List;
-
-public interface MobilityRepository extends ODHRepository {
-    String findBySnameEquals(String sname);
-    String findBySoriginIn(List<String> sorigin);
-}
